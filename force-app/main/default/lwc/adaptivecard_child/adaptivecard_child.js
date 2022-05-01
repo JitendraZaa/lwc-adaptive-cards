0dfc877c163ba6fd9102dea9b185fc31ee5433e1
Jitendra Zaa <JitendraZaa@users.noreply.github.com>
2022-04-30 21:54:17 -0400

    Working example of Adaptive Card & LWC

--- force-app/main/default/lwc/adaptivecard_child/adaptivecard_child.js
+++ force-app/main/default/lwc/adaptivecard_child/adaptivecard_child.js
@@ -12,35 +12,31 @@ export default class Adaptivecard_child extends LightningElement {
             // Create an AdaptiveCard instance
             //var adaptiveCardObj = new adaptivecardsLib.AdaptiveCard(); 
             var adaptiveCard = new AdaptiveCards.AdaptiveCard();
-  
+            //Let adaptive card know that its running context is LWC
             adaptiveCard.lwcRef = this;
 
            
             adaptiveCard.onExecuteAction = function(action)  {  
-                //console.log('clicked 1');
-                //console.log(action.url); 
-                //console.log(document); 
-                //console.log(action); 
-                //console.log(action.parent); 
-                //console.log(action.parent.lwcRef); 
-                console.log(action.parent.lwcRef.customText); 
+                //Get LWC runtime back from Adaptive card instance
+                var getBackLWC = action.parent.lwcRef ;
+
+                console.log(getBackLWC.customText); 
+                console.log(action.url); 
+                console.log(action.type); 
+                console.log(action.title); 
             }
             
             adaptiveCard.parse(this.card);
             // Render the card to an HTML element:
             var renderedCard = adaptiveCard.render();
             const el = this.template.querySelector('div');
-            el.appendChild(renderedCard);
-            console.log('Hello World');
+            el.appendChild(renderedCard); 
         });
     }
 
     handleFormInputChange(event){
         // In 1 line, assign the value to the property
-        this.customText = event.target.value;
-        if(this.adaptiveCard){
-            this.adaptiveCard.lwcRef = this; 
-        } 
+        this.customText = event.target.value; 
     }
 
     card = {
